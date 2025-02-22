5a248dc73f0fa3e1fc8c2006dbf332565eba64b1
Connor Abbas <abbasconnor@gmail.com>
2025-02-22 21:31:37 +0000

    edit layout

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -25,55 +25,52 @@ defineOptions({ layout: AuthenticatedLayout });
                 Profile
             </template>
         </PageTitleSection>
-        <div>
-            <div class="space-y-4 md:space-y-8">
-                <Card
-                    pt:body:class="max-w-2xl space-y-3"
-                    pt:caption:class="space-y-1"
-                >
-                    <template #title>Profile Information</template>
-                    <template #subtitle>
-                        Update your account's profile information and email
-                        address.
-                    </template>
-                    <template #content>
-                        <UpdateProfileInformationForm
-                            :must-verify-email="mustVerifyEmail"
-                            :status="status"
-                        />
-                    </template>
-                </Card>
 
-                <Card
-                    pt:body:class="max-w-2xl space-y-3"
-                    pt:caption:class="space-y-1"
-                >
-                    <template #title>Update Password</template>
-                    <template #subtitle>
-                        Ensure your account is using a long, random password
-                        to stay secure.
-                    </template>
-                    <template #content>
-                        <UpdatePasswordForm />
-                    </template>
-                </Card>
+        <Card
+            pt:body:class="max-w-2xl space-y-3"
+            pt:caption:class="space-y-1"
+        >
+            <template #title>Profile Information</template>
+            <template #subtitle>
+                Update your account's profile information and email
+                address.
+            </template>
+            <template #content>
+                <UpdateProfileInformationForm
+                    :must-verify-email="mustVerifyEmail"
+                    :status="status"
+                />
+            </template>
+        </Card>
 
-                <Card
-                    pt:body:class="max-w-2xl space-y-3"
-                    pt:caption:class="space-y-1"
-                >
-                    <template #title>Delete Account</template>
-                    <template #subtitle>
-                        Once your account is deleted, all of its resources
-                        and data will be permanently deleted. Before
-                        deleting your account, please download any data or
-                        information that you wish to retain.
-                    </template>
-                    <template #content>
-                        <DeleteUserForm />
-                    </template>
-                </Card>
-            </div>
-        </div>
+        <Card
+            pt:body:class="max-w-2xl space-y-3"
+            pt:caption:class="space-y-1"
+        >
+            <template #title>Update Password</template>
+            <template #subtitle>
+                Ensure your account is using a long, random password
+                to stay secure.
+            </template>
+            <template #content>
+                <UpdatePasswordForm />
+            </template>
+        </Card>
+
+        <Card
+            pt:body:class="max-w-2xl space-y-3"
+            pt:caption:class="space-y-1"
+        >
+            <template #title>Delete Account</template>
+            <template #subtitle>
+                Once your account is deleted, all of its resources
+                and data will be permanently deleted. Before
+                deleting your account, please download any data or
+                information that you wish to retain.
+            </template>
+            <template #content>
+                <DeleteUserForm />
+            </template>
+        </Card>
     </Container>
 </template>
