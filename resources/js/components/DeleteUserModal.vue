75d0daaceffe78957ef60c5894e4d3458f70ebb6
Connor Abbas <abbasconnor@gmail.com>
2025-03-17 01:38:58 +0000

    password inputs

--- resources/js/components/DeleteUserModal.vue
+++ resources/js/components/DeleteUserModal.vue
@@ -49,13 +49,14 @@ function focusPasswordInput() {
                 id="password"
                 ref="password-input"
                 v-model="form.password"
+                :invalid="Boolean(form.errors.password)"
                 type="password"
                 placeholder="Password"
+                autocomplete="current-password"
+                toggleMask
                 autofocus
                 required
                 fluid
-                :invalid="Boolean(form.errors.password)"
-                autocomplete="current-password"
                 @keyup.enter="deleteUser"
             />
             <Message
