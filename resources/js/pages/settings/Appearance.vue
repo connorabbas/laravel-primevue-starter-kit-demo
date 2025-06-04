7ecb85d2bcda5d1aecc8e3b7116f0d797d39a54e
Connor Abbas <abbasconnor@gmail.com>
2025-06-03 20:01:17 -0500

    fix: sentence case label

--- resources/js/pages/settings/Appearance.vue
+++ resources/js/pages/settings/Appearance.vue
@@ -28,7 +28,7 @@ const breadcrumbs = [
                 <template #content>
                     <div class="space-y-6">
                         <div class="flex flex-col gap-2">
-                            <label for="color-mode-selector">Color Mode</label>
+                            <label for="color-mode-selector">Color mode</label>
                             <SelectColorModeButton id="color-mode-selector" />
                         </div>
                         <div class="flex flex-col gap-2">
