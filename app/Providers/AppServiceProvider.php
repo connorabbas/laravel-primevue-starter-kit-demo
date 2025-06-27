f8e30f0e5d28a2c7faf7d6f23ec750b611004d6f
Connor Abbas <abbasconnor@gmail.com>
2025-06-27 12:14:50 +0000

    log viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,13 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+
+        LogViewer::auth(function ($request) {
+            if (app()->isLocal()) {
+                return true;
+            }
+
+            return $request?->user()->hasRole('Admin');
+        });
     }
 }
