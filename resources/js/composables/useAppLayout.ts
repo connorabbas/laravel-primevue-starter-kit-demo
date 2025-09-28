609d36d44a26fb5d5e07d62898d4f513846d796b
Connor Abbas <abbasconnor@gmail.com>
2025-09-28 17:48:45 +0000

    improvements: TS changes

--- resources/js/composables/useAppLayout.ts
+++ resources/js/composables/useAppLayout.ts
@@ -1,9 +1,9 @@
-import { ref, computed, onMounted, onUnmounted, watchEffect } from 'vue';
-import { usePage, useForm } from '@inertiajs/vue3';
+import { ref, computed, onMounted, onUnmounted, watchEffect } from 'vue'
+import { usePage, useForm } from '@inertiajs/vue3'
 import {
     LayoutGrid, House, Info, Settings, LogOut, ExternalLink, FileSearch, FolderGit2, Lock, Users
-} from 'lucide-vue-next';
-import { MenuItem } from '@/types';
+} from 'lucide-vue-next'
+import { MenuItem } from '@/types'
 
 export function useAppLayout() {
     const page = usePage()
@@ -16,73 +16,69 @@ export function useAppLayout() {
     })
 
     // Menu items
-    const menuItems = computed<MenuItem[]>(() => {
-        const items: MenuItem[] = [
-            {
-                label: 'Home',
-                lucideIcon: House,
-                route: route('welcome'),
-                active: currentRoute.value == 'welcome',
-            },
-            {
-                label: 'Dashboard',
-                lucideIcon: LayoutGrid,
-                route: route('dashboard'),
-                active: currentRoute.value == 'dashboard',
-            },
-            {
-                label: 'Resources',
-                lucideIcon: Info,
-                items: [
-                    {
-                        label: 'Laravel Docs',
-                        url: 'https://laravel.com/docs/master',
-                        target: '_blank',
-                        lucideIcon: ExternalLink,
-                    },
-                    {
-                        label: 'PrimeVue Docs',
-                        url: 'https://primevue.org/',
-                        target: '_blank',
-                        lucideIcon: ExternalLink,
-                    },
-                    {
-                        label: 'Starter Kit Docs',
-                        url: 'https://connorabbas.github.io/laravel-primevue-starter-kit-docs/',
-                        target: '_blank',
-                        lucideIcon: FileSearch,
-                    },
-                    {
-                        label: 'Starter Kit Repo',
-                        url: 'https://github.com/connorabbas/laravel-primevue-starter-kit',
-                        target: '_blank',
-                        lucideIcon: FolderGit2,
-                    },
-                ],
-            },
-            {
-                label: 'Admin',
-                lucideIcon: Lock,
-                visible: page.props.auth.isAdmin,
-                items: [
-                    {
-                        label: 'Dashboard',
-                        lucideIcon: LayoutGrid,
-                        route: route('admin.dashboard'),
-                        active: currentRoute.value == 'admin.dashboard',
-                    },
-                    {
-                        label: 'Users',
-                        lucideIcon: Users,
-                        route: route('admin.users.index'),
-                        active: currentRoute.value == 'admin.users.index',
-                    },
-                ],
-            },
-        ];
-
-        return items;
-    });
+    const menuItems = computed<MenuItem[]>(() => [
+        {
+            label: 'Home',
+            lucideIcon: House,
+            route: route('welcome'),
+            active: currentRoute.value == 'welcome',
+        },
+        {
+            label: 'Dashboard',
+            lucideIcon: LayoutGrid,
+            route: route('dashboard'),
+            active: currentRoute.value == 'dashboard',
+        },
+        {
+            label: 'Resources',
+            lucideIcon: Info,
+            items: [
+                {
+                    label: 'Laravel Docs',
+                    url: 'https://laravel.com/docs/master',
+                    target: '_blank',
+                    lucideIcon: ExternalLink,
+                },
+                {
+                    label: 'PrimeVue Docs',
+                    url: 'https://primevue.org/',
+                    target: '_blank',
+                    lucideIcon: ExternalLink,
+                },
+                {
+                    label: 'Starter Kit Docs',
+                    url: 'https://connorabbas.github.io/laravel-primevue-starter-kit-docs/',
+                    target: '_blank',
+                    lucideIcon: FileSearch,
+                },
+                {
+                    label: 'Starter Kit Repo',
+                    url: 'https://github.com/connorabbas/laravel-primevue-starter-kit',
+                    target: '_blank',
+                    lucideIcon: FolderGit2,
+                },
+            ],
+        },
+        {
+            label: 'Admin',
+            lucideIcon: Lock,
+            visible: page.props.auth.isAdmin,
+            items: [
+                {
+                    label: 'Dashboard',
+                    lucideIcon: LayoutGrid,
+                    route: route('admin.dashboard'),
+                    active: currentRoute.value == 'admin.dashboard',
+                },
+                {
+                    label: 'Users',
+                    lucideIcon: Users,
+                    route: route('admin.users.index'),
+                    active: currentRoute.value == 'admin.users.index',
+                },
+            ],
+        },
+    ])
 
     // User menu and logout functionality.
     const logoutForm = useForm({})
